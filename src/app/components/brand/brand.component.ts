23799feb3b2a29b9d49f4ce18995d1952db85291
enes <dere.enes01@gmail.com>
2021-03-13 19:28:20 +0300

    init

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Brand } from 'src/app/models/brand';
 import { BrandService } from 'src/app/services/brand.service';
+import { CarDtoService } from 'src/app/services/car-dto.service';
 import { ColorService } from 'src/app/services/color.service';
 
 @Component({
@@ -12,19 +13,21 @@ import { ColorService } from 'src/app/services/color.service';
 export class BrandComponent implements OnInit {
   brands:Brand[]=[];
   dataLoaded=false;
-  constructor(private brandService:BrandService) { }
+  constructor(private brandService:BrandService,
+    private carDtoService:CarDtoService
+    ) { }
 
   ngOnInit(): void {
     this.getBrands();
   }
   handleClick(brand:any){
-    console.log(brand.id);
+    this.carDtoService.getByBrand(brand.id)
   }
   getBrands(){
     this.brandService.getBrands().subscribe(response=>{
       this.brands=response.data
-      console.log(this.brands)
-      this.dataLoaded=true;
+      //console.log(this.brands)
+     // this.dataLoaded=true;
 
     })
   }
